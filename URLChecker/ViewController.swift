49601c0591dfac272a1923885d16ab09bab7491b
Saeed Hajizade <saeed.hjz90@gmail.com>
2023-09-23 04:03:47 +0330

    Initial Commit

---
+++ URLChecker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLChecker
+//
+//  Created by Saeed on 7/1/1402 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
